0628b982c9443da8d47ddfa281267c94bf823233
FreeCoderKR <cystmax@gmail.com>
2022-03-01 22:30:44 +0900

    FEAT : refresh add

---
+++ pages/api/auth/refresh.js
@@ -0,0 +1,48 @@
+const { sign, verify, refreshVerify } = require("../../../lib/jwt");
+const jwt = require("jsonwebtoken");
+
+const refresh = async (req, res) => {
+  if (req.headers.authorization && req.headers.refresh) {
+    const authToken = req.headers.authorization.split("Bearer ")[1];
+    const refreshToken = req.headers.refresh;
+    // access token 검증 -> expired여야 함.
+    const authResult = verify(authToken);
+    const decoded = jwt.decode(authToken);
+    if (decoded === null) {
+      res.status(401).send({
+        success: false,
+        message: "No authorized!",
+      });
+    }
+    const refreshResult = refreshVerify(refreshToken, decoded.email);
+    if (authResult.success === false && authResult.message === "jwt expired") {
+      if (refreshResult.success === false) {
+        res.status(401).send({
+          success: false,
+          message: "No authorized!",
+        });
+      } else {
+        const newAccessToken = sign(user);
+        res.status(200).send({
+          // 새로 발급한 access token과 원래 있던 refresh token 모두 클라이언트에게 반환합니다.
+          ok: true,
+          data: {
+            accessToken: newAccessToken,
+            refreshToken,
+          },
+        });
+      }
+    } else {
+      res.status(400).send({
+        success: false,
+        message: "Acess token is not expired!",
+      });
+    }
+  } else {
+    res.status(400).send({
+      success: false,
+      message: "Access token and refresh token are need for refresh!",
+    });
+  }
+};
+module.exports = refresh;
